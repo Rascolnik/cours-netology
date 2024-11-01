2b1741eb3bab7ce24e4a197541676b8858ae4863
Dmitriy Glaznitskiy <glaznickiy@gmail.com>
2024-11-01 16:29:55 +0300

    initial

--- .Основы языка программирования Python/3.Введение в типы данных/3.3 Задание «Срезы строк».py
+++
@@ -1,9 +0,0 @@
-def string_slices(string: str) -> str:
-    return string[2:-2]
-
-
-if __name__ == '__main__':
-    assert string_slices("%%Приказ об увольнении&#") == 'Приказ об увольнении'
-    assert string_slices("%%Лучший студент на курсе!&#") == 'Лучший студент на курсе!'
-    assert string_slices("%%Hello World!&#") == 'Hello World!'
-    print("\nОтличная работа, отправляйте на проверку!")
--- Stepik/Какая следующая буква 🔜.py
+++
@@ -1,9 +0,0 @@
-litter = ord(input())
-
-next_litter = litter + 1
-
-if next_litter <= ord("Я") :
-    print(chr(next_litter))
-
-else:
-    print("Дальше букв нет")
